d1fd0250e9da2af5344f057b75c4db253ef95d7d
Joshua Haddock <joshhaddock88@gmail.com>
2021-05-18 18:49:05 -0700

    Make renderChart function. Automatically remove eventlisteners from viewResults upton button click

--- js/app.js
+++ js/app.js
@@ -73,6 +73,62 @@ function displayVoteCount() {
     liElem.textContent = `${product.name}: ${product.clicks}`;
     results.appendChild(liElem);
   }
+  viewResults.removeEventListener('click', displayVoteCount);
+}
+
+function renderChart() {
+  let labelData = [];
+  for(let i = 0; i < Product.allImages.length; i++) {
+    let product = Product.allImages[i];
+    labelData.push(product.name);
+  }
+  let voteData = [];
+  for(let product of Product.allImages) {
+    voteData.push(product.clicks);
+  }
+  console.log(labelData);
+  console.log(voteData);
+
+  let ctx = document.getElementById('productChart').getContext('2d');
+  let productChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labelData,
+        datasets: [{
+            label: '# of Votes',
+            data: voteData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+  });
+  viewResults.removeEventListener('click', renderChart);
 }
 
 function handleClick(event) {
@@ -105,6 +161,7 @@ function handleClick(event) {
 
 allProducts.addEventListener('click', handleClick);
 viewResults.addEventListener('click', displayVoteCount);
+viewResults.addEventListener('click', renderChart);
 
 // --------------------------------------------- Functions Calls -------------------------------------------------------//
 
